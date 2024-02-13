3bceb599d458e3bc54bfb69b8dee7bb918d53aa8
Danil Bogomolov <danilbogomolov@Danils-MacBook-Pro.local>
2024-02-13 12:29:27 +0300

    Initial commit

---
+++ src/main/java/com/skypro/ShelterPetTelegramBot/ShelterPetTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.ShelterPetTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterPetTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterPetTelegramBotApplication.class, args);
+	}
+
+}
