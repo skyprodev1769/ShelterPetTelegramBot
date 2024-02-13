99de7855b50a797487a0bad546a6cf1f3ac52145
danilbogomolov <skyprodev1769@gmail.com>
2024-02-13 12:34:26 +0300

    dev_branch

--- src/main/java/com/skypro/ShelterPetTelegramBot/ShelterPetTelegramBotApplication.java
+++ src/main/java/com/skypro/ShelterPetTelegramBot/ShelterPetTelegramBotApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShelterPetTelegramBotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShelterPetTelegramBotApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterPetTelegramBotApplication.class, args);
+
+	}
 
 }
