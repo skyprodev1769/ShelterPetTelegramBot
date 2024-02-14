17968a22458b9a13908d062c14d9ef3f408cb9cf
IVANPOTEMKIN <vanek.potemkin@gmail.com>
2024-02-14 15:51:55 +0300

    Создал свою ветку (Иван).

--- src/test/java/com/skypro/ShelterPetTelegramBot/ShelterPetTelegramBotApplicationTests.java
+++ src/test/java/com/skypro/ShelterPetTelegramBot/ShelterPetTelegramBotApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShelterPetTelegramBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
