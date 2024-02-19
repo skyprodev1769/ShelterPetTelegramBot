729c4f3dc8bfb7dbba4d162bd111ad0405644e9d
PACKOJIbHUKOB <jordn-5@yandex.ru>
2024-02-19 18:11:52 +0300

    добавил к ваниному коду возможность отправлять фото и документы в вызовах информации о приюте ( расписание работы приюта, адрес и схема проезда, и полные правила поведения и техника безопасности на территории приюта)

--- src/main/java/com/skypro/ShelterPetTelegramBot/service/TelegramBot.java
+++ src/main/java/com/skypro/ShelterPetTelegramBot/service/TelegramBot.java
@@ -5,12 +5,17 @@ import com.skypro.ShelterPetTelegramBot.model.PotentialParent;
 import com.skypro.ShelterPetTelegramBot.model.User;
 import com.skypro.ShelterPetTelegramBot.model.repository.PotentialParentRepository;
 import com.skypro.ShelterPetTelegramBot.model.repository.UserRepository;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
@@ -20,6 +25,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -179,6 +185,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             case INFO_ABOUT_WORK_SCHEDULE_AND_ADDRESS:
                 answer = REACTION_TO_INFO_ABOUT_WORK_SCHEDULE_AND_ADDRESS;
                 reactionToCommand(chatId, answer);
+                sendFoto(chatId,"staticFile/foto/adres.jpg");
                 break;
 
             case INFO_ABOUT_SECURITY_CONTACT_DETAILS:
@@ -189,6 +196,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             case INFO_ABOUT_GENERAL_SAFETY_RECOMMENDATION:
                 answer = REACTION_TO_INFO_ABOUT_GENERAL_SAFETY_RECOMMENDATION;
                 reactionToCommand(chatId, answer);
+                sendDocument(chatId,"полные правила поведения и техника безопасности на территории приюта смотрите в документе","staticFile/docs/Safety_precautions.txt");
                 break;
 
             case RECORD_CONTACT_DETAILS:
@@ -615,5 +623,33 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error("ERROR: {}", e.getMessage());
         }
+    }
+    private void sendDocument(long chatId,String caption, String documentPath) {
+        SendDocument sendDocument = new SendDocument();
+        sendDocument.setChatId(chatId);
+        sendDocument.setCaption(caption);
+        sendDocument.setDocument(transformsFilePathToInputFile(documentPath));
+        try {
+            execute(sendDocument);
+        } catch (TelegramApiException e) {
+            log.error("ERROR: {}", e.getMessage());
+        }
+    }
+
+    private void sendFoto(long chatId, String fotoPath){
+        SendPhoto sendfoto = new SendPhoto();
+        sendfoto.setChatId(chatId);
+        sendfoto.setPhoto(transformsFilePathToInputFile(fotoPath));
+        try {
+            execute(sendfoto);
+        } catch (TelegramApiException e) {
+            log.error("ERROR: {}", e.getMessage());
+        }
+    }
+    @SneakyThrows
+    private InputFile transformsFilePathToInputFile(String path){
+        File file = ResourceUtils.getFile("classpath:" + path);
+        InputFile sendFile = new InputFile(file);
+        return sendFile;
     }
 }
