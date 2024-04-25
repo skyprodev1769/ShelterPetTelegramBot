ee07bfa69ca7c3cf15cd845efca71959b8ec16f7
danilbogomolov <skyprodev1769@gmail.com>
2024-04-25 22:20:45 +0300

    123

--- src/main/java/com/skypro/ShelterPetTelegramBot/utils/answers/AnswerForParents.java
+++ src/main/java/com/skypro/ShelterPetTelegramBot/utils/answers/AnswerForParents.java
@@ -23,7 +23,8 @@ public class AnswerForParents {
         return String.format("""
                 %s %s,
                 Ваш номер телефона - %s
-                Вы взяли опеку над животным - %s                               
+                Вы взяли опеку над животным - %s     
+                                          
                 Было принято решение продлить Ваш испытательный срок на 30 дней!""", firstName, lastName, phoneNumber, petName);
     }
 
